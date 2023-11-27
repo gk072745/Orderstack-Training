7c95490b3364168466939c8a0163328d77bf109a
gk072745 <kumawatgovind222@gmail.com>
2023-11-27 15:10:48 +0530

    variable declartion and value assign revised

---
+++ JavaScript_Fundamentals–Part_1/Values_and_Variables.js
@@ -0,0 +1,19 @@
+// Declare variables called 'country', 'continent' and 'population' and
+// assign their values according to your own country (population in millions)
+
+let country;
+let continent;
+let population;
+
+country = "India";
+continent = "Asia";
+population = "130+ Cr";
+
+// 2. Log their values to the console
+// first way
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// second way
+console.log(country + "\n" + continent + "\n" + population);
