3d3fb7820e30e0822d8961f2538d76244a264324
gk072745 <kumawatgovind222@gmail.com>
2023-11-27 16:05:31 +0530

    datatypes concepts revised

---
+++ JavaScript_Fundamentals–Part_1/dataTypes.js
@@ -0,0 +1,21 @@
+// Declare a variable called 'isIsland' and set its value according to your
+// country. The variable should hold a Boolean value. Also declare a variable
+// 'language', but don't assign it any value yet
+
+let isIsland = false;
+
+let language;
+
+// Log the types of 'isIsland', 'population', 'country' and 'language'
+// to the console
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
+let country = "India";
+let continent = "Asia";
+let population = "1300+ million";
+
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
