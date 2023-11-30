f1a0d1bd40810e1c4ab3a70cd0752d85774fa924
gk072745 <kumawatgovind222@gmail.com>
2023-11-30 16:20:59 +0530

    updated pattern

--- pattern.js
+++ pattern.js
@@ -1,34 +1,39 @@
 // // Problem statement: Given a number n, write a JavaScript program to print a diamond star pattern of 2n - 1 rows.
 
 let n = 5;
-for (let i = 0; i < 2 * n - 1; i++) {
-  let patternLine = "";
-  let flag = false;
 
-  for (let j = 0; j < n * 2 - 1; j++) {
-    if (i < n) {
-      if (j < n - i - 1 || j > n + i - 1) {
-        patternLine += " ";
-      } else {
-        if (flag) {
+function diamondPattern(n) {
+  for (let i = 0; i < 2 * n - 1; i++) {
+    let patternLine = "";
+    let flag = false;
+
+    for (let j = 0; j < n * 2 - 1; j++) {
+      if (i < n) {
+        if (j < n - i - 1 || j > n + i - 1) {
           patternLine += " ";
         } else {
-          patternLine += "*";
+          if (flag) {
+            patternLine += " ";
+          } else {
+            patternLine += "*";
+          }
+          flag = !flag;
         }
-        flag = !flag;
-      }
-    } else {
-      if (j < i - n + 1 || j > 2 * n + 2 - i) {
-        patternLine += " ";
       } else {
-        if (flag) {
+        if (j < i - n + 1 || j > 2 * n + 2 - i) {
           patternLine += " ";
         } else {
-          patternLine += "*";
+          if (flag) {
+            patternLine += " ";
+          } else {
+            patternLine += "*";
+          }
+          flag = !flag;
         }
-        flag = !flag;
       }
     }
+    console.log(patternLine);
   }
-  console.log(patternLine);
 }
+
+diamondPattern(5);
