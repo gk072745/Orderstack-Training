945933a81704e7fdfc9b5be572c46961e4ad991a
gk072745 <kumawatgovind222@gmail.com>
2023-12-03 18:44:01 +0530

    linkedlist implementation added

---
+++ Linked_List--implementation.js
@@ -0,0 +1,35 @@
+// single node creation
+class Node {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+// constructor for linkedlist
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  add(data) {
+    let node = new Node(data);
+
+    if (this.head === null) {
+      this.head = node;
+    } else {
+      let currNode = this.head;
+
+      while (currNode.next) {
+        currNode = currNode.next;
+      }
+
+      currNode.next = node;
+    }
+  }
+}
+
+const linkedlist = new LinkedList();
+linkedlist.add(1);
+linkedlist.add(2);
+console.log(linkedlist);
