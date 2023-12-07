d57d516fb500dc9caa467471270551f5a8bd9db5
gk072745 <kumawatgovind222@gmail.com>
2023-12-07 10:25:06 +0530

    nested obj counting done

---
+++ Nested_Object_Counting.js
@@ -0,0 +1,52 @@
+const company = {
+  id: 1,
+  name: "John Doe",
+  role: "CEO",
+  subordinates: [
+    {
+      id: 2,
+      name: "Alice Johnson",
+      role: "Manager",
+      subordinates: [
+        {
+          id: 3,
+          name: "Bob Smith",
+          role: "Team Lead",
+          subordinates: [
+            {
+              id: 4,
+              name: "Charlie Brown",
+              role: "Developer",
+              subordinates: [],
+            },
+            {
+              id: 5,
+              name: "David Lee",
+              role: "Developer",
+              subordinates: [],
+            },
+          ],
+        },
+        {
+          id: 6,
+          name: "Eve Taylor",
+          role: "Team Lead",
+          subordinates: [],
+        },
+      ],
+    },
+  ],
+};
+
+const countedEmp = {};
+
+const countEmployeesByRole = ({ role, subordinates }) => {
+
+  countedEmp[role] = (countedEmp[role] || 0) + 1;
+
+  subordinates.forEach(subords => countEmployeesByRole(subords));
+
+  return countedEmp;
+};
+
+console.log(countEmployeesByRole(company));
