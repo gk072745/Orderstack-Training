dae8b41747c1963cb3ad108bb98aeb5167c93db3
gk072745 <kumawatgovind222@gmail.com>
2023-12-07 19:40:58 +0530

    some binary tree methods added

--- binary_tree.js
+++ binary_tree.js
@@ -13,66 +13,105 @@ class BinaryTree {
     this.head = null;
   }
 
-  // method for new node insertion
-  add(data) {
+  // methods
+  // interting ele. in the binary tree...
+  insert(data) {
+    const node = new Node(data)
+    const currNode = this.head
 
-    let node = new Node(data);
-
-    if (this.head === null) this.head = node;
-    else {
-      let currNode = this.head;
-
-      //condition for  lesser and equeal data
-      if (data <= currNode.data) {
-
-        currNode.leftNode
-          ? this.traverseNodes(currNode.leftNode, data, node)
-          : (currNode.leftNode = node);
-
-        //condition for  greater data
-      } else {
-
-        currNode.rightNode
-          ? this.traverseNodes(currNode.rightNode, data)
-          : (currNode.rightNode = node);
-
-      }
-    }
+    if (this.head == null) this.head = node
+    else this.traverseNodes(node, currNode)
+    return this.head
   }
 
-  //method for traversing nodes
-  traverseNodes(currNode, data, newNode) {
+  //recursive function for inserting element
+  traverseNodes(node, currNode) {
+    const { data } = node
+
 
     //condition for traversing left node
     if (data <= currNode.data) {
 
       currNode.leftNode
-        ? this.traverseNodes(currNode.leftNode, data, newNode)
-        : (currNode.leftNode = newNode);
+        ? this.traverseNodes(node, currNode.leftNode)
+        : (currNode.leftNode = node);
 
       //condition for traversing right node
     } else {
 
       currNode.rightNode
-        ? this.traverseNodes(node.currNode.rightNode, data, newNode)
-        : (currNode.rightNode = newNode);
+        ? this.traverseNodes(node, currNode.rightNode)
+        : (currNode.rightNode = node);
 
     }
   }
+
+
+
+  // search single node
+  searchNode(data, currNode = this.head) {
+    if (currNode === null || data === currNode.data) return currNode
+    return data < currNode.data ? this.searchNode(data, currNode.leftNode) : this.searchNode(data, currNode.rightNode)
+  }
+
+  //traversal tree methods
+
+  // 1. Inorder Traversal
+  inorderTraversal(currNode = this.head) {
+    if (currNode === null) return
+
+    this.inorderTraversal(currNode.leftNode)
+    console.log(currNode.data)
+    this.inorderTraversal(currNode.rightNode)
+  }
+
+  // 2. preorder Traversal
+  preorderTraversal(currNode = this.head) {
+    if (currNode === null) return
+
+    console.log(currNode.data)
+    this.preorderTraversal(currNode.leftNode)
+    this.preorderTraversal(currNode.rightNode)
+  }
+
+  // 3. postorder Traversal
+  postorderTraversal(currNode = this.head) {
+    if (currNode === null) return
+
+    this.postorderTraversal(currNode.leftNode)
+    this.postorderTraversal(currNode.rightNode)
+    console.log(currNode.data)
+
+  }
+
+  // delete single node
+  deleteNode(data, currNode = this.head) {
+    if (currNode === null || data === undefined) return
+
+    const searchedNode = currNode
+    while (currNode.leftNode || currNode.rightNode) {
+
+    }
+
+
+
+  }
+
 }
 
 const bst = new BinaryTree();
 
-bst.add(7);
-bst.add(5);
-bst.add(4);
-bst.add(9);
-bst.add(2);
-bst.add(2);
-bst.add(2);
-bst.add(11);
-bst.add(17);
-bst.add(18);
-bst.add(6);
+bst.insert(7);
+bst.insert(4);
+bst.insert(5);
+bst.insert(9);
+bst.insert(2);
+bst.insert(3);
+bst.insert(2);
+bst.insert(11);
+bst.insert(17);
+bst.insert(16);
+bst.insert(6);
 
-console.log(bst);
+bst.deleteNode(2)
+bst.inorderTraversal()
