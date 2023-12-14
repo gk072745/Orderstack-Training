84745b5dcdf529c9fc757be9ddabb278a30db693
gk072745 <kumawatgovind222@gmail.com>
2023-12-14 12:28:40 +0530

    right way of adding methods for event listeners

--- Vue/basics-03-events-starting-code/app.js
+++ Vue/basics-03-events-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  // we write methods not for funcitons but also we add methods for like vue call when we need.
+  methods: {
+    add() {
+      this.counter++
+    },
+
+    reduce() {
+      this.counter--
+    }
+  }
 });
 
 app.mount('#events');
