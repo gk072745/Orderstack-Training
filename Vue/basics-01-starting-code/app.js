ad2fca4be5d30ff633be73d91682a698594273e3
gk072745 <kumawatgovind222@gmail.com>
2023-12-14 14:43:07 +0530

    practice ass. 2

--- Vue/basics-01-starting-code/app.js
+++ Vue/basics-01-starting-code/app.js
@@ -1,21 +1,16 @@
-
 const app = Vue.createApp({
     data() {
         return {
-            // storing data.............we can store any data here and that will visible on dom.
-            courseGoalA: "<h1>Finish the course and learn Vue!</h1>",
-            courseGoalB: "Master Vue and build amazing apps!</h1>",
-            // now i want to send a link as data....
-            vueLink: "https://vuejs.org/"
+            courseGoal: "learn vue",
+            vueLink: "https://vuejs.org"
         };
     },
+
     methods: {
-        outputGoal() {
-            const randomNum = Math.random();
-            return randomNum < 0.5 ?
-                this.courseGoalA : this.courseGoalB;
-        }
-    }
-});
+
+    },
+
+})
 
 app.mount("#user-goal")
+
