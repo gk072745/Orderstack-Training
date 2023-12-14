74e29d3c5de640778763614e95a32885151d2406
gk072745 <kumawatgovind222@gmail.com>
2023-12-14 09:49:14 +0530

    atrribute inding leart

--- Vue/basics-01-starting-code/app.js
+++ Vue/basics-01-starting-code/app.js
@@ -4,6 +4,8 @@ const app = Vue.createApp({
         return {
             // storing data.............we can store any data here and that will visible on dom.
             courseGoal: "Finish the course and learn Vue!",
+            // now i want to send a link as data....
+            vueLink: "https://vuejs.org/"
         };
     }
 });
