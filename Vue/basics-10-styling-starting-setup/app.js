0cace4023be52a6d5d5b27d9f7972979ebe1add4
gk072745 <kumawatgovind222@gmail.com>
2023-12-15 12:20:54 +0530

    dynamic styling

---
+++ Vue/basics-10-styling-starting-setup/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            boxASelected: false,
+            boxBSelected: false,
+            boxCSelected: false,
+        }
+    },
+    methods: {
+        boxSelected(box) {
+            console.log(box)
+            if (box === "A") this.boxASelected = !this.boxASelected
+            if (box === "B") this.boxBSelected = true
+            if (box === "C") this.boxCSelected = true
+        },
+    }
+
+});
+
+app.mount("#styling")
