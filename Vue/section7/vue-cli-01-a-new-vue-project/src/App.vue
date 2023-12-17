16e75b8e4b9fc9f7434dbdd6c6d57115e9b28276
gk072745 <kumawatgovind222@gmail.com>
2023-12-17 15:48:29 +0530

    added

---
+++ Vue/section7/vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h2>My friends</h2>
+    <ul>
+      <friend-contact> </friend-contact>
+      <friend-contact> </friend-contact>
+    </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from "./components/FriendContact.vue";
+export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Lorenz",
+          phone: "12323-13123-123",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "manuel2",
+          name: "Manuel2 Lorenz",
+          phone: "12323-13123-123",
+          email: "manuel2@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ Vue/section7/vue-first-app/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    requireConfigFile: false, // <== ADD THIS
+    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  }
+}
