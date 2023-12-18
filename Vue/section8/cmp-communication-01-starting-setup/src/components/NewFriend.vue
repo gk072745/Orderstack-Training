41ddf5e2ce5f6ebdb763eff1e5af3c5a19665ecb
gk072745 <kumawatgovind222@gmail.com>
2023-12-18 19:19:01 +0530

    new component crud

---
+++ Vue/section8/cmp-communication-01-starting-setup/src/components/NewFriend.vue
@@ -0,0 +1,62 @@
+<template>
+  <form @submit.prevent="submitData">
+    <div>
+      <label for="">Name</label>
+      <input
+        type="text"
+        v-model="enteredName" />
+    </div>
+    <div>
+      <label for="">Phone</label>
+      <input
+        type="tel"
+        v-model="enteredPhone" />
+    </div>
+    <div>
+      <label for="">Email</label>
+      <input
+        type="email"
+        v-model="enteredEmail" />
+    </div>
+    <button>Add Contact</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      enteredName: "",
+      enteredPhone: "",
+      enteredEmail: "",
+    };
+  },
+  emits: ["add-contact"],
+  methods: {
+    submitData() {
+      this.$emit(
+        "add-contact",
+        this.enteredName,
+        this.enteredEmail,
+        this.enteredPhone
+      );
+    },
+  },
+};
+</script>
+
+<style>
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
+}
+</style>
