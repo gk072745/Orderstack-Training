c0fc0520144ac5234a2b67352471e97f2507db21
gk072745 <kumawatgovind222@gmail.com>
2023-12-18 15:06:21 +0530

    passing data to childs

---
+++ Vue/section8/cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,34 @@
+<template>
+  <li :key="id">
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phone }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ email }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["id", "name", "phone", "email"],
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
