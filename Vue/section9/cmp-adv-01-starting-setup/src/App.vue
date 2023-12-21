1908ef536329fdc62390344cd274cbb7ec4774f0
gk072745 <kumawatgovind222@gmail.com>
2023-12-21 13:17:07 +0530

    slots

---
+++ Vue/section9/cmp-adv-01-starting-setup/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- <the-header></the-header>
+      both will work -->
+    <TheHeader />
+    <badge-list></badge-list>
+    <user-info
+      :full-name="activeUser.name"
+      :info-text="activeUser.description"
+      :role="activeUser.role"></user-info>
+  </div>
+</template>
+
+<script>
+// import header from "./components/TheHeader.vue";
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+
+export default {
+  // components: { "the-header": header },
+  components: { TheHeader, BadgeList, UserInfo },
+  data() {
+    return {
+      activeUser: {
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
