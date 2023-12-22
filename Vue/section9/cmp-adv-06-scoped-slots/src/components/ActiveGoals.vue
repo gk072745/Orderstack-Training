d2534fd1694c9bb350371a0f66d3edd083bad672
gk072745 <kumawatgovind222@gmail.com>
2023-12-22 08:55:56 +0530

    slots,dynamic componenets,teleport

---
+++ Vue/section9/cmp-adv-06-scoped-slots/src/components/ActiveGoals.vue
@@ -0,0 +1,3 @@
+<template>
+  <h2>active golas</h2>
+</template>
---
+++ Vue/section9/cmp-adv-06-scoped-slots/src/components/ErrorAlert.vue
@@ -0,0 +1,19 @@
+<template>
+  <dialog open>
+    <slot> </slot>
+  </dialog>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+dialog {
+  margin: 0;
+  position: fixed;
+  top: 20vh;
+  left: 30%;
+  width: 5;
+}
+</style>
